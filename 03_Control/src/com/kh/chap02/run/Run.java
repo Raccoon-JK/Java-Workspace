e769becbe4b45f173895123ae02a6ee98a846cb6
Raccoon-JK <110690389+Raccoon-JK@users.noreply.github.com>
2022-11-14 14:56:06 +0900

    자바 분기문
    자바 분기문

--- 03_Control/src/com/kh/chap02/run/Run.java
+++ 03_Control/src/com/kh/chap02/run/Run.java
@@ -34,7 +34,20 @@ public class Run {
 		//lp.practice9();
 		//lp.practice10();
 		//lp.practice11();
-		lp.practice12();
+		//lp.practice12();
+		//lp.practice14();
+		//lp.practice18();
+		
+		
+	B_While bw = new B_While();
+		//bw.method1();
+		//bw.method2();
+		//bw.method3();
+		//bw.method4();
+		//bw.method5();
+		//bw.method6();
+		//bw.method7();
+	
 	}
 	
 
