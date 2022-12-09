0d8bff6108bc0f8a6eb2336337860a9c15333c98
Raccoon-JK <110690389+Raccoon-JK@users.noreply.github.com>
2022-12-09 09:17:34 +0900

    12/8 study
    12/8 study

---
+++ 14_GUI/src/com/kh/chap03_event/view/Test.java
@@ -0,0 +1,11 @@
+package com.kh.chap03_event.view;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Test implements ActionListener {
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("haha");
+	}
+}
